b38d25ba2f63806279ad1474abd7ee53a27c1f18
onehunga <julian@michels.hamburg>
2024-05-21 16:15:00 +0200

    navigator history, can return to previos pages

--- src/main/java/oop/lernquiz/navigator/Navigator.java
+++ src/main/java/oop/lernquiz/navigator/Navigator.java
@@ -1,15 +1,17 @@
 package oop.lernquiz.navigator;
 
-/**
- * TODO: implement history
- */
+import java.util.Stack;
+
 public class Navigator {
+	private static final Stack<Integer> history = new Stack<>();
 	private static ViewController currentController;
 	private static final ViewControllerFactory[] factories = {new HomeViewControllerFactory(), new SchwachstellenViewControllerFactory(), new QuizStartenViewControllerFactory()};
 
+	private static int currentControllerIndex;
+
 	public static void init() {
 		// die Zahl im index beschreibt die Startseite
-		currentController = factories[0].createViewController(null);
+		setNavigation(0, null);
 	}
 
 	public static void navigateTo(String target) {
@@ -17,13 +19,39 @@ public class Navigator {
 	}
 
 	public static void navigateTo(String target, NavigatorProperties properties) {
-		currentController.controller().close();
-		currentController.view().dispose();
-
-		for (var factory : factories) {
+		for (int i = 0; i < factories.length; i++) {
+			var factory = factories[i];
 			if (factory.getRouteName().equals(target)) {
-				currentController = factory.createViewController(properties);
+				history.add(currentControllerIndex);
+				setNavigation(i, properties);
+				break;
 			}
 		}
+	}
+
+	/**
+	 * Keine Fehlermeldungen wenn, da ein leerer stack bedeutet, dass man auf der startseite ist.
+	 */
+	public static void goBack() {
+		if (history.isEmpty()) {
+			return;
+		}
+
+		var last = history.pop();
+		setNavigation(last, null);
+	}
+
+	private static void setNavigation(int target, NavigatorProperties properties) {
+		if (target >= factories.length) {
+			return;
+		}
+
+		if (currentController != null) {
+			currentController.controller().close();
+			currentController.view().dispose();
+		}
+
+		currentController = factories[target].createViewController(properties);
+		currentControllerIndex = target;
 	}
 }
